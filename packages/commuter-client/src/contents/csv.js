298278d9a73f995c4c01ed844704d8ede66c8a90
Kyle Kelley <rgbkrk@gmail.com>
2017-04-13 18:05:01 -0700

    rely on the data resource for rendering
    need to work on the styling

--- packages/commuter-client/src/contents/csv.js
+++ packages/commuter-client/src/contents/csv.js
@@ -1,6 +1,32 @@
 const d3 = Object.assign({}, require("d3-dsv"));
 
 import React from "react";
+import DataTransform from "@nteract/transform-dataresource";
+
+import "react-virtualized/styles.css";
+
+import { infer } from "jsontableschema";
+
+const SAMPLE_SIZE = 10;
+
+function getSampleRows(data, sampleSize) {
+  return Array.from({ length: Math.min(sampleSize, data.length) }, () => {
+    const index = Math.floor(Math.random() * data.length);
+    return data[index];
+  });
+}
+
+function inferSchema(data) {
+  const sampleRows = getSampleRows(data, SAMPLE_SIZE);
+  const headers = Array.from(
+    sampleRows.reduce(
+      (result, row) => new Set([...result, ...Object.keys(row)]),
+      new Set()
+    )
+  );
+  const values = sampleRows.map(row => Object.values(row));
+  return infer(headers, values);
+}
 
 export default class CSVView extends React.Component {
   shouldComponentUpdate() {
@@ -8,6 +34,12 @@ export default class CSVView extends React.Component {
   }
 
   render() {
-    return <pre>No CSV rendering yet!</pre>;
+    const data = d3.csvParse(this.props.entry.content);
+    const schema = inferSchema(data);
+    return (
+      <div>
+        <DataTransform data={{ data, schema }} theme="light" />
+      </div>
+    );
   }
 }
