cb7631b906fef6b75f1691e2c7d3bca003a5a883
Kyle Kelley <rgbkrk@gmail.com>
2017-04-21 13:26:02 -0700

    begin adopting router creator

--- packages/commuter-server/src/routes/api/contents.js
+++ packages/commuter-server/src/routes/api/contents.js
@@ -2,49 +2,56 @@
 
 import type { $Request, $Response } from "express";
 
-const express = require("express"),
-  router = express.Router(),
-  { isDir } = require("../util"),
-  {
-    listObjects,
-    getObject,
-    deleteObject,
-    deleteObjects,
-    uploadObject
-  } = require("./../../services/s3");
+const express = require("express");
+
+const {
+  listObjects,
+  getObject,
+  deleteObject,
+  deleteObjects,
+  uploadObject
+} = require("./../../services/s3");
+
+const { isDir } = require("../util");
 
 const errObject = (err, path) => ({
   message: `${err.message}: ${path}`,
   reason: err.code
 });
 
-router.get("/*", (req: $Request, res: $Response) => {
-  const path = req.params["0"];
-  const cb = (err, data) => {
-    if (err) res.status(500).json(errObject(err, path));
-    else res.json(data);
-  };
-  if (isDir(path)) listObjects(path, cb);
-  else getObject(path, cb);
-});
+function createRouter(): express.Router {
+  const router = express.Router();
 
-router.delete("/*", (req: $Request, res: $Response) => {
-  const path = req.params["0"];
-  const cb = (err, data) => {
-    if (err) res.status(500).json(errObject(err, path));
-    else res.status(204).send(); //as per jupyter contents api
-  };
-  if (isDir(path)) deleteObjects(path, cb);
-  else deleteObject(path, cb);
-});
+  router.get("/*", (req: $Request, res: $Response) => {
+    const path = req.params["0"];
+    const cb = (err, data) => {
+      if (err) res.status(500).json(errObject(err, path));
+      else res.json(data);
+    };
+    if (isDir(path)) listObjects(path, cb);
+    else getObject(path, cb);
+  });
 
-router.post("/*", (req: $Request, res: $Response) => {
-  const path = req.params["0"];
-  const cb = (err, data) => {
-    if (err) res.status(500).json(errObject(err, path));
-    else res.status(201).send();
-  };
-  uploadObject(path, req.body, cb);
-});
+  router.delete("/*", (req: $Request, res: $Response) => {
+    const path = req.params["0"];
+    const cb = (err, data) => {
+      if (err) res.status(500).json(errObject(err, path));
+      else res.status(204).send(); //as per jupyter contents api
+    };
+    if (isDir(path)) deleteObjects(path, cb);
+    else deleteObject(path, cb);
+  });
 
-module.exports = router;
+  router.post("/*", (req: $Request, res: $Response) => {
+    const path = req.params["0"];
+    const cb = (err, data) => {
+      if (err) res.status(500).json(errObject(err, path));
+      else res.status(201).send();
+    };
+    uploadObject(path, req.body, cb);
+  });
+
+  return router;
+}
+
+module.exports = createRouter();
