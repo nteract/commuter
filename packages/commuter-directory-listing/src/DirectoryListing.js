9e99c0499e0eb8577a492168f11f409fc95ebab0
Kyle Kelley <rgbkrk@gmail.com>
2017-04-22 22:37:35 -0700

    make directory listing match the contents API paths

--- packages/commuter-directory-listing/src/DirectoryListing.js
+++ packages/commuter-directory-listing/src/DirectoryListing.js
@@ -4,13 +4,12 @@ import { Table, Grid, Segment, Icon } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 
 const DirectoryListing = props => {
-  const handleClick = path =>
-    e => {
-      if (props.onClick) {
-        e.preventDefault();
-        props.onClick(path);
-      }
-    };
+  const handleClick = path => e => {
+    if (props.onClick) {
+      e.preventDefault();
+      props.onClick(path);
+    }
+  };
   const base = props.basepath;
   return (
     <Grid>
@@ -21,46 +20,45 @@ const DirectoryListing = props => {
           </Table.Header>
           <Table.Body>
             {props.contents.map((row, index) => {
-              const fullPath = `${base}${row.path}`;
-
-              switch (row.type) {
-                case "notebook":
-                  return (
-                    <Table.Row key={index}>
-                      <Table.Cell>
-                        <a href={fullPath} onClick={handleClick(fullPath)}>
-                          <Icon name="book" color="grey" />{row.name}
-                        </a>
-                      </Table.Cell>
-                      <Table.Cell collapsing textAlign="right" />
-                    </Table.Row>
-                  );
-                case "directory":
-                  return (
-                    <Table.Row key={index}>
-                      <Table.Cell collapsing>
-                        <a href={fullPath} onClick={handleClick(fullPath)}>
-                          <Icon name="folder" color="blue" />{row.name}
-                        </a>
-                      </Table.Cell>
-                      <Table.Cell collapsing textAlign="right" />
-                    </Table.Row>
-                  );
-                case "file":
-                  return (
-                    <Table.Row key={index}>
-                      <Table.Cell collapsing>
-                        <a href={fullPath} onClick={handleClick(fullPath)}>
-                          <Icon name="file" color="grey" />{row.name}
-                        </a>
-                      </Table.Cell>
-                      <Table.Cell collapsing textAlign="right" />
-                    </Table.Row>
-                  );
-                default:
-                  return null;
-              }
-            })}
+              const fullPath = `${base}/${row.path}`;
+                switch (row.type) {
+                  case "notebook":
+                    return (
+                      <Table.Row key={index}>
+                        <Table.Cell>
+                          <a href={fullPath} onClick={handleClick(fullPath)}>
+                            <Icon name="book" color="grey" />{row.name}
+                          </a>
+                        </Table.Cell>
+                        <Table.Cell collapsing textAlign="right" />
+                      </Table.Row>
+                    );
+                  case "directory":
+                    return (
+                      <Table.Row key={index}>
+                        <Table.Cell collapsing>
+                          <a href={fullPath} onClick={handleClick(fullPath)}>
+                            <Icon name="folder" color="blue" />{row.name}
+                          </a>
+                        </Table.Cell>
+                        <Table.Cell collapsing textAlign="right" />
+                      </Table.Row>
+                    );
+                  case "file":
+                    return (
+                      <Table.Row key={index}>
+                        <Table.Cell collapsing>
+                          <a href={fullPath} onClick={handleClick(fullPath)}>
+                            <Icon name="file" color="grey" />{row.name}
+                          </a>
+                        </Table.Cell>
+                        <Table.Cell collapsing textAlign="right" />
+                      </Table.Row>
+                    );
+                  default:
+                    return null;
+                }
+              })}
           </Table.Body>
         </Table>
       </Grid.Column>
