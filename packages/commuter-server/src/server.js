2bbff8e190f47806828be0a654078875515d7c3f
Kyle Kelley <rgbkrk@gmail.com>
2017-07-01 17:18:23 -0700

    begin invocation of next.js

--- packages/commuter-server/src/server.js
+++ packages/commuter-server/src/server.js
@@ -12,28 +12,31 @@ const next = require("next");
 
 const dev = process.env.NODE_ENV !== "production";
 const nextApp = next({ dev });
+const handle = nextApp.getRequestHandler();
 
 function createServer() {
-  const app = express();
-  app.use(morgan("common"));
+  return nextApp.prepare().then(() => {
+    const app = express();
+    app.use(morgan("common"));
 
-  app.use(express.static("static"));
+    app.use(express.static("static"));
 
-  log.info(`Node env: ${config.nodeEnv}`);
+    log.info(`Node env: ${config.nodeEnv}`);
 
-  app.use(
-    "/nteract/commuter",
-    express.static(path.resolve(__dirname, "..", "build"))
-  );
+    app.use(
+      "/nteract/commuter",
+      express.static(path.resolve(__dirname, "..", "build"))
+    );
 
-  // Last middleware
-  app.use(require("./routes"));
+    // Last middleware
+    app.use(require("./routes"));
 
-  const server = http.createServer(app);
+    const server = http.createServer(app);
 
-  return new Promise(accept => {
-    server.listen(config.port, () => {
-      accept(server);
+    return new Promise(accept => {
+      server.listen(config.port, () => {
+        accept(server);
+      });
     });
   });
 }
