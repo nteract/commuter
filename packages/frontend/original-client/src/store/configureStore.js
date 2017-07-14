4fb51c22099229a6af291e1b24ce42fd1c80d01a
Kyle Kelley <rgbkrk@gmail.com>
2017-07-14 13:21:12 -0700

    clean up unused files from original client (#199)

--- packages/frontend/original-client/src/store/configureStore.js
+++
@@ -1,8 +0,0 @@
-import { createStore, applyMiddleware } from "redux";
-import thunkMiddleware from "redux-thunk";
-import rootReducer from "../reducers";
-
-const configureStore = preloadedState =>
-  createStore(rootReducer, preloadedState, applyMiddleware(thunkMiddleware));
-
-export default configureStore;
--- packages/frontend/original-client/src/store/preloadedState.js
+++
@@ -1,6 +0,0 @@
-const INITIAL_STATE = {
-  commuter: { entry: { type: "directory", content: [] }, isFetching: false },
-  discovery: { discovered: [] }
-};
-
-export default INITIAL_STATE;
