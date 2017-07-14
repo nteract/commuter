2779ff73bb3926123cc082f900bf7b609e7064b0
Kyle Kelley <rgbkrk@gmail.com>
2017-07-14 11:05:42 -0700

    write more informative comments

--- packages/commuter-server/src/server.js
+++ packages/commuter-server/src/server.js
@@ -23,14 +23,19 @@ function createServer() {
 
     log.info(`Node env: ${config.nodeEnv}`);
 
-    // Last middleware
     const router = require("./routes");
 
+    /**
+     *
+     * /view/ router
+     *
+     * On some filetypes it will serve the next.js app and on others it will
+     * redirect to the literal file. This is to allow for resolving relative
+     * assets like images within notebooks.
+     */
     const suffixRegex = /(?:\.([^.]+))?$/;
-
     const renderSuffixes = new Set(["ipynb", "html", "json", "md", "rmd"]);
     const renderAccepts = new Set(["text/html", "application/xhtml+xml"]);
-
     const viewHandler = (req: $Request, res: $Response) => {
       const suffix = (suffixRegex.exec(req.path)[1] || "").toLowerCase();
       const accepts = (req.headers.accept || "").split(",");
@@ -54,22 +59,22 @@ function createServer() {
       return;
     };
 
-    router.get(["/view", "/view*"], viewHandler);
-
-    // Hokey pokey passthrough for now
-    router.get("*", (req: $Request, res: $Response) => {
+    /**
+     * Express middleware for letting our next.js frontend do the handling
+     */
+    const passToNext = (req: $Request, res: $Response) => {
       return frontend.handle(req, res);
-    });
+    };
+
+    router.get(["/view", "/view*"], viewHandler);
+    router.get("*", passToNext);
 
     // TODO: Leaving this here for the eventual baseURL handling
     const baseURI = "/";
     app.use(baseURI, router);
-
-    // Hokey pokey passthrough for now -- all other request go through next
+    // TODO: This is duplicate until we're doing proper baseURL handling
     app.get(["/view", "/view*"], viewHandler);
-    app.use((req: $Request, res: $Response) => {
-      return frontend.handle(req, res);
-    });
+    app.use(passToNext);
 
     const server = http.createServer(app);
 
