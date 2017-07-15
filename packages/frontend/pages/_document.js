4c7c840d227f3d0bea19576c6fa83fb36fc2641c
Lukas Geiger <lukas.geiger94@gmail.com>
2017-07-15 16:53:03 +0200

    Take out leftover styled-jsx
    It was removed as a dependency in #200

--- packages/frontend/pages/_document.js
+++ packages/frontend/pages/_document.js
@@ -1,12 +1,9 @@
 import Document, { Head, Main, NextScript } from "next/document";
 
-import flush from "styled-jsx/server";
-
 export default class MyDocument extends Document {
   static getInitialProps({ renderPage }) {
     const { html, head, errorHtml, chunks } = renderPage();
-    const styles = flush();
-    return { html, head, errorHtml, chunks, styles };
+    return { html, head, errorHtml, chunks };
   }
 
   render() {
