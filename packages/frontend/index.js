61ccb7055d70be1c5931bcdfaace22ff89840f70
Kyle Kelley <rgbkrk@gmail.com>
2017-07-15 21:21:18 -0700

    flow type all the "pages"

--- packages/frontend/index.js
+++ packages/frontend/index.js
@@ -1,3 +1,5 @@
+// Note: this module must remain compatible with the ECMAScript version of the
+// Server as it does _not_ get transpiled
 const next = require("next");
 const dev = process.env.NODE_ENV !== "production" && !process.env.NOW;
 
--- packages/frontend/pages/view.js
+++ packages/frontend/pages/view.js
@@ -1,3 +1,4 @@
+// @flow
 import React from "react";
 import Link from "next/link";
 require("isomorphic-fetch");
