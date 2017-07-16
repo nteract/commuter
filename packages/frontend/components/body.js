53c8aa9acc6c009b725ecc323a811c30b2fdd55c
Kyle Kelley <rgbkrk@gmail.com>
2017-07-15 20:39:42 -0700

    flow type many of the components

--- packages/frontend/components/body.js
+++ packages/frontend/components/body.js
@@ -1,6 +1,12 @@
+// @flow
 import { Menu, Image, Container } from "semantic-ui-react";
 
-const Body = props => {
+type BodyProps = {
+  // children: any // literally what's in the flow libdef, we expect a React Element
+  children: React$Element<*>
+};
+
+const Body = (props: BodyProps) => {
   return (
     <div>
       <div className="main-container">
