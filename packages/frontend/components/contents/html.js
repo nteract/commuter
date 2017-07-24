58b0a7d2680ad8b003cced4dd3f725a7f025f228
Kyle Kelley <rgbkrk@gmail.com>
2017-07-24 09:32:46 -0700

    set the height based on the scrollHeight of iframe
    Note: this isn't working yet due to the iframe being in a null origin.
    I wanted to post the PR so I remember to come back to this.

--- packages/frontend/components/contents/html.js
+++ packages/frontend/components/contents/html.js
@@ -5,19 +5,25 @@ import React from "react";
 export default class HTMLView extends React.Component {
   ifr: HTMLIFrameElement;
 
+  componentDidMount() {
+    const el = this.ifr;
+    this.ifr.onload = () => {
+      const height =
+        (el.contentDocument &&
+          el.contentDocument.body &&
+          el.contentDocument.body.scrollHeight) ||
+        "400";
+      el.style.height = `${height}px`;
+    };
+  }
+
   shouldComponentUpdate() {
     return false;
   }
 
   render() {
     return (
-      <div
-        style={{
-          position: "fixed",
-          width: "100%",
-          height: "100%"
-        }}
-      >
+      <div style={{}}>
         <iframe
           sandbox="allow-scripts"
           style={{
@@ -25,15 +31,12 @@ export default class HTMLView extends React.Component {
             height: "100%",
             border: "none",
             margin: "0",
-            padding: "0",
-            display: "block"
+            padding: "0"
           }}
           srcDoc={this.props.entry.content}
           ref={f => {
             this.ifr = f;
           }}
-          height="100%"
-          width="100%"
         />
       </div>
     );
