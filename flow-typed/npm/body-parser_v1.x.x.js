c84137d285b18eb387b1d11bde6b58b0f8b1e4c9
Kyle Kelley <rgbkrk@gmail.com>
2017-09-03 16:48:27 -0700

    update all the flow defs across all packages

--- flow-typed/npm/body-parser_v1.x.x.js
+++ flow-typed/npm/body-parser_v1.x.x.js
@@ -1,46 +1,43 @@
-// flow-typed signature: c0ce3f9bac97fe109ad556ad8c5abb41
-// flow-typed version: 0b37536688/body-parser_v1.x.x/flow_>=v0.30.x
+// flow-typed signature: 18dadbe162b608c79b9b31c3d2f1c822
+// flow-typed version: b43dff3e0e/body-parser_v1.x.x/flow_>=v0.17.x
 
-import type { Middleware, $Request, $Response } from "express";
+import type { Middleware, $Request, $Response } from 'express';
 
 declare type bodyParser$Options = {
-  inflate?: boolean,
-  limit?: number | string,
-  type?: string | string[] | ((req: $Request) => any),
-  verify?: (
-    req: $Request,
-    res: $Response,
-    buf: Buffer,
-    encoding: string
-  ) => void
+  inflate?: boolean;
+  limit?: number | string;
+  type?: string | string[] | ((req: $Request) => any);
+  verify?: (req: $Request, res: $Response, buf: Buffer, encoding: string) => void;
 };
 
 declare type bodyParser$OptionsText = bodyParser$Options & {
-  reviver?: (key: string, value: any) => any,
-  strict?: boolean
+  reviver?: (key: string, value: any) => any;
+  strict?: boolean;
 };
 
 declare type bodyParser$OptionsJson = bodyParser$Options & {
-  reviver?: (key: string, value: any) => any,
-  strict?: boolean
+  reviver?: (key: string, value: any) => any;
+  strict?: boolean;
 };
 
 declare type bodyParser$OptionsUrlencoded = bodyParser$Options & {
-  extended?: boolean,
-  parameterLimit?: number
+  extended?: boolean;
+  parameterLimit?: number;
 };
 
 declare module "body-parser" {
-  declare type Options = bodyParser$Options;
-  declare type OptionsText = bodyParser$OptionsText;
-  declare type OptionsJson = bodyParser$OptionsJson;
-  declare type OptionsUrlencoded = bodyParser$OptionsUrlencoded;
 
-  declare function json(options?: OptionsJson): Middleware;
+    declare type Options = bodyParser$Options;
+    declare type OptionsText = bodyParser$OptionsText;
+    declare type OptionsJson = bodyParser$OptionsJson;
+    declare type OptionsUrlencoded = bodyParser$OptionsUrlencoded;
 
-  declare function raw(options?: Options): Middleware;
+    declare function json(options?: OptionsJson): Middleware;
 
-  declare function text(options?: OptionsText): Middleware;
+    declare function raw(options?: Options): Middleware;
 
-  declare function urlencoded(options?: OptionsUrlencoded): Middleware;
+    declare function text(options?: OptionsText): Middleware;
+
+    declare function urlencoded(options?: OptionsUrlencoded): Middleware;
+
 }
