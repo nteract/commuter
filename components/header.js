55575f8bccdb050f2d56b06804c2a76b1af7c4b0
M Pacer <mpacer@berkeley.edu>
2019-02-13 14:11:16 -0800

    switch header to styled-components

--- components/header.js
+++ components/header.js
@@ -3,12 +3,73 @@ import * as React from "react";
 import Link from "next/link";
 import NProgress from "nprogress";
 import Router from "next/router";
+import {createGlobalStyle} from "styled-components"
 
 Router.onRouteChangeStart = () => {
   NProgress.start();
 };
 Router.onRouteChangeComplete = () => NProgress.done();
 Router.onRouteChangeError = () => NProgress.done();
+
+const GlobalStyle = createGlobalStyle`
+  nav {
+    background: ${theme.background};
+    border: 1px solid ${theme.outline};
+    font-family: "Source Sans Pro";
+    font-size: 16px;
+    color: ${theme.primary};
+    padding-left: 1.5em;
+  }
+
+  img {
+    padding-right: 1em;
+  }
+
+  ul {
+    display: flex;
+    width: 100%;
+    position: relative;
+
+    margin: 0;
+    padding: 0;
+    text-align: center;
+
+    list-style: none;
+  }
+
+  ul li {
+    flex-direction: row;
+    list-style-type: none;
+    display: inline;
+    text-align: center;
+    display: flex;
+    align-items: center;
+  }
+
+  ul li a {
+    vertical-align: middle;
+    display: table;
+    padding: 1em;
+    color: ${theme.primary};
+    text-decoration: none;
+  }
+
+  ul li.active a {
+    color: ${theme.active};
+    font-weight: 500;
+    text-decoration: none;
+    cursor: default;
+  }
+
+  a:hover {
+    text-decoration: underline;
+  }
+
+  img {
+    height: 2.5rem;
+    vertical-align: middle;
+  }
+`
 
 type ActiveType = "view" | "discover";
 
@@ -35,6 +96,7 @@ class CommuterMenu extends React.Component<*> {
   render() {
     return (
       <nav className="main-header">
+        <GlobalStyle/>
         <ul className="items">
           <li>
             <Link href={"/view"}>
@@ -54,65 +116,6 @@ class CommuterMenu extends React.Component<*> {
             </li>
           ) : null}
         </ul>
-        <style jsx>{`
-          nav {
-            background: ${theme.background};
-            border: 1px solid ${theme.outline};
-            font-family: "Source Sans Pro";
-            font-size: 16px;
-            color: ${theme.primary};
-            padding-left: 1.5em;
-          }
-
-          img {
-            padding-right: 1em;
-          }
-
-          ul {
-            display: flex;
-            width: 100%;
-            position: relative;
-
-            margin: 0;
-            padding: 0;
-            text-align: center;
-
-            list-style: none;
-          }
-
-          ul li {
-            flex-direction: row;
-            list-style-type: none;
-            display: inline;
-            text-align: center;
-            display: flex;
-            align-items: center;
-          }
-
-          ul li a {
-            vertical-align: middle;
-            display: table;
-            padding: 1em;
-            color: ${theme.primary};
-            text-decoration: none;
-          }
-
-          ul li.active a {
-            color: ${theme.active};
-            font-weight: 500;
-            text-decoration: none;
-            cursor: default;
-          }
-
-          a:hover {
-            text-decoration: underline;
-          }
-
-          img {
-            height: 2.5rem;
-            vertical-align: middle;
-          }
-        `}</style>
       </nav>
     );
   }
