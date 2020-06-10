38c98274d6c04b5632a365996a9a5f1cc7ff8136
Greg Roodt <greg@canva.com>
2020-06-10 17:25:35 +1000

    path.join

--- src/frontend.js
+++ src/frontend.js
@@ -1,11 +1,12 @@
 // Note: this module must remain compatible with the ECMAScript version of the
 // Server as it does _not_ get transpiled
 const next = require("next");
+const path = require("path");
 
 const dev = process.env.NODE_ENV !== "production" && !process.env.NOW;
 
 function createNextApp() {
-  const app = next({ dev, dir: `${__dirname}/../` });
+  const app = next({ dev, dir: path.join(__dirname, "..") });
   const handle = app.getRequestHandler();
 
   return {
